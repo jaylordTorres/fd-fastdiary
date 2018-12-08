aebee3024440f9dc8fbab274d2142b4303166a7b
Jay Lord P. Torres <isens.jaylord@gmail.com>
2018-12-08 12:08:36 +0800

    partial signin

--- src/lib/auth/container/SignIn.container.js
+++ src/lib/auth/container/SignIn.container.js
@@ -4,31 +4,59 @@ import React, { Component } from 'react';
 
 import {
   StyleSheet,
+  Image
 } from 'react-native';
 import { 
-  View, Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text
+  Container, Form, Input, Item, Label, Button, Icon, Text
 } from 'native-base'
+import Wallpaper from './../component/Wallpaper'
+import { util } from './../../../service/api'
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {}
+    this._login = this._login.bind(this)
+  }
+
+  componentDidMount () {
+  }
+
+  _login () {
+
+  }
+
   render() {
     return (
-      <Container>
-        <Content>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full onPress={()=> this._onSignIn()}>
-              <Text>facebook login</Text>
+      <Container style={styles.container}>
+      <Wallpaper style={styles.wallpaper}>
+        <Form style={styles.form}>
+            <Item error>
+            <Icon active name='lock' />
+              <Input placeholder={'Access Code'}/>
+            </Item>
+            <Button full onPress={this._login}>
+              <Text>login</Text>
             </Button>
-          </FooterTab>
-        </Footer>
+          </Form>
+        </Wallpaper>
       </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'black'
+  },
+  wallpaper: {
+    alignContent: 'center', justifyContent: 'center'
+  },
+  form: {
+    backgroundColor: 'white',
 
+  }
 });
 
 
--- src/lib/auth/screen/SignIn.screen.js
+++ src/lib/auth/screen/SignIn.screen.js
@@ -1,10 +1,25 @@
-'use strict';
-
 import React from 'react';
+import { connect } from 'react-redux'
 import SignIn from './../container/SignIn.container'
+import UserActions from './../../../redux-saga/redux/user.redux'
+import SettingActions from '../../../redux-saga/redux/setting.redux'
 
-const SignInScreen = (props) => {
-    return <SignIn />
+export const SignInScreen = (props) => {
+    return <SignIn {...props} />
 }
 
-export default SignInScreen;
+const msp = (state) => {
+  return {
+    user: state.user.user,
+  }
+}
+
+const mdp = (dispatch) => {
+  return {
+  	loginUser: (param, cb) => dispatch(UserActions.loginUser(param, cb)),
+  	start: (param, cb) => dispatch(SettingActions.start(param, cb)),
+  }
+}
+
+export default connect(msp, mdp)(SignInScreen)
+
