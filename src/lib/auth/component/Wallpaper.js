9401deaa62f1d1fd9a070f8136ea5c26611276ba
Jay Lord P. Torres <isens.jaylord@gmail.com>
2018-12-08 12:09:47 +0800

    Wallpaper

---
+++ src/lib/auth/component/Wallpaper.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import {
+	StyleSheet,
+} from 'react-native'
+import Image from 'react-native-image-progress';
+import Loading from './../../../shared/Loading'
+import { images } from './../../../style/global'
+
+const Wallpaper = ({uri, children, style ={}}) => {
+	const image = uri ? {uri} : images.defaultBackground
+	return (
+      <Image style={[styles.img, style]} source={image} indicator={Loading}>{children || null}</Image>
+    )
+}
+
+const styles = StyleSheet.create({
+	img: {
+	  backgroundColor: 'black',
+	  flex: 1,
+	  resizeMode: 'stretch',
+	}
+});
+
+export default Wallpaper;
